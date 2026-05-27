16f8070c2028fbe21b48563e9d3993c11f1b090a
sergiopaz-bit <sergio.paz@udea.edu.co>
2026-05-26 21:08:13 -0500

    Update 07-sample.ts

--- lgbti-chronicles-app/src/app/core/data/chronicles/thematic-3/07-sample.ts
+++ lgbti-chronicles-app/src/app/core/data/chronicles/thematic-3/07-sample.ts
@@ -2,24 +2,25 @@ import type { LocalizedModalContent } from '../../../models/content.model';
 
 export const CHRONICLE_T3_07: LocalizedModalContent = {
   title: {
-    es: 'Crónica T3-07 (muestra)',
-    fr: 'Chronique T3-07 (échantillon)',
-    en: 'Chronicle T3-07 (sample)',
+    es: 'Rafaela',
+    fr: 'Rafaela',
+    en: 'Rafaela',
   },
   rawText: {
-    es: 'Texto de muestra para la crónica T3-07. {1}Frase destacada{/1} con una nota de ejemplo.',
-    fr: `Texte d’échantillon pour la chronique T3-07. {1}Phrase mise en évidence{/1} avec une note \
-d’exemple.`,
-    en: 'Sample text for chronicle T3-07. {1}Highlighted phrase{/1} with an example tooltip.',
+    es: `<p>«Porque quería salir como mujer trans pa podernos maquillar, vestir, taconear. Yo recuerdo muy bien los desfiles, los reinados, entre nosotras mismas, pero encerradas... entonces yo me iba para la cancha, para el parque, a jugar baloncesto o voleibol con las maricas y allá recochábamos. Allá era donde planeábamos, en las tribunas de la cancha, quién era la próxima en ir al reinado, porque íbamos a concursos a Urabá. Estaban la Mario, la Pablo, la Karina. Había varias y dijimos que ese año íbamos a ser Karina y yo, entonces la Mario y la Pablo dijeron: “Nosotras vamos para el próximo”. Nos turnábamos. Para ese año llegó la Karina y dijo: “Nos vamos a organizar, vamos a practicar, vamos a entrenar”. Hablamos con la Wildimar, que era la organizadora de eso, y organizamos todo. A mí me hicieron la invitación, la echamos a la maleta y nos fuimos a los reinados de ahí p’abajo: estuvimos en Mutatá, en Apartadó, en Turbo y en Necoclí... Nos recorrimos cuatro pasarelas».</p><p>Mujer trans, lideresa</i></p>`,
+    fr: `<p>« Je faisais tout ça parce que je voulais sortir en tant que femme trans, avec du maquillage, des vêtements de femme, des talons. Je me souviens très bien des défilés, des concours, mais nous faisions ça entre nous, enfermées... Alors, j’allais au terrain ou au parc pour jouer au basket ou au volley avec les autres pédés et nous nous amusions là-bas. C’était dans les gradins où nous planifiions qui était la prochaine à concourir, nous allions au concours d’Urabá. Nous étions beaucoup : miss Mario, miss Pablo, miss Karina. Nous avions décidé que cette année-là miss Karina et moi serions les choisies pour y participer, alors miss Mario et miss Pablo ont dit : “Nous irons au prochain”. Nous alternions. Cette année-là, miss Karina est arrivée et nous a dit : “Nous allons nous organiser, nous allons pratiquer, nous allons nous entraîner”. Nous avons parlé avec miss Wildimar qui était l’organisatrice du concours pour que tout soit prêt. On m’a donné l’invitation, nous l’avons mise dans la valise et nous sommes allées à tous les concours de la région : nous avons été à Mutatá, à Apartadó, à Turbo et à Necoclí... Nous avons défilé dans quatre concours ».</p><p><i>Femme trans, leader</i></p>`,
+    en: `<p>“We wanted to go out as trans women so that we could wear make-up, dress up, walk around in heels. I remember so well the fashion shows and the pageants that we used to put on behind closed doors. I’d go to the court or to the park to play basketball or volleyball with the rest of the fags, and there we’d mess around. There, in the tribunes of the court is where we’d plan who was the next to go to the pageon, because we used to go to pageants in Urabá. There, we had Miss Mario, Miss Pablo, Miss Karina. That year we decided that Karina and I were the ones to go, so Miss Mario and Miss Pablo said, ‘we will go next year’. We took turns. That same year, Karina said: ‘Let’s get it together, let’s rehearse, let’s get ready’. We talked to Miss Wildimar, who was the person in charge, and organized everything. Once they sent me the invitation, I packed my bags, and we went to all the pageants in the region: we went to Mutatá, Apartadó, Turbo, Necoclí...we went to four fashion shows.”</p><p><i>Trans woman, leader</i></p>`,
   },
   tooltips: {
     1: {
-      es: '<p>Nota de ejemplo para la crónica T3-07.</p>',
-      fr: '<p>Note d’exemple pour la chronique T3-07.</p>',
-      en: '<p>Sample tooltip for chronicle T3-07.</p>',
+      es: `<p>Nota de ejemplo para la crónica T3-07.</p>`,
+      fr: `<p>Note d’exemple pour la chronique T3-07.</p>`,
+      en: `<p>Sample tooltip for chronicle T3-07.</p>`,
     },
   },
   contextInfo: {
-    es: '<p>year: 2025</p><p>actor: FARC EP</p><p>location: Arauca - Arauquita</p>',
+    es: `<p>Lugar: Urabá, Antioquia</p><p>Año: N/A</p><p>Hecho(s) victimizante(s): N/A</p><p>Actor armado: N/A</p>`,
+    fr: `<p>Lugar: Urabá, Antioquia</p><p>Año: N/A</p><p>Hecho(s) victimizante(s): N/A</p><p>Actor armado: N/A</p>`,
+    en: `<p>Lugar: Urabá, Antioquia</p><p>Año: N/A</p><p>Hecho(s) victimizante(s): N/A</p><p>Actor armado: N/A</p>`,
   },
 };
