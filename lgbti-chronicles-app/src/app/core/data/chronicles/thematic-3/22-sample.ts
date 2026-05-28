cb05ee81427829778eb02e836d09c201beaa51c3
sergiopaz-bit <sergio.paz@udea.edu.co>
2026-05-28 16:28:44 -0500

    Update 22-sample.ts

--- lgbti-chronicles-app/src/app/core/data/chronicles/thematic-3/22-sample.ts
+++ lgbti-chronicles-app/src/app/core/data/chronicles/thematic-3/22-sample.ts
@@ -2,25 +2,31 @@ import type { LocalizedModalContent } from '../../../models/content.model';
 
 export const CHRONICLE_T3_22: LocalizedModalContent = {
   title: {
-    es: 'Rogelia',
-    fr: 'Rogelia',
-    en: 'Rogelia',
+    es: 'Daniela',
+    fr: 'Daniela',
+    en: 'Daniela',
   },
   rawText: {
-    es: `<p>«En Bogotá hay cerezos, árboles medicinales que nos han servido para curar nuestras enfermedades y ayudarles a otros. [...] Para algunas mujeres, la llegada a Bogotá abrió sus horizontes al desarrollo de sus liderazgos o potenció los que ya traían; en otros casos, la ciudad mimetizó esas potencialidades y ellas entraron a la lucha por la sobrevivencia, como todas, formando parte de la economía informal, que es la que se ofrece a las mujeres de los barrios populares de la ciudad».</p><p><i>Mujer víctima</i></p>`,
-    fr: `<p>« Dans Bogotá, il y a des arbres avec des vertus médicinales comme les cerisiers, ils nous ont aidés à guérir nos maladies mais aussi à aider d’autres personnes. […] Pour certaines femmes, Bogotá leur a ouvert de nouveaux horizons afin d’établir ou d’agrandir leur <i>leadership</i>. Mais pour d’autres, la ville n’a pas présente autant d’opportunité. Parce que pour pouvoir survivre, les femmes ont dû lutter pour leurs vies. Elles l’ont fait en travaillant au noir comme la majorité d’entre nous. On ne nous offre que ce type de travail, à nous, les femmes qui habitons dans les quartiers populaires. »</p><p><i>Femme victime</i></p>`,
-    en: `<p>“In Bogotá, there are cherry trees. These are medicinal trees that have helped us cure our diseases and help others. […] For some women, their arrival to Bogotá opened their horizons to new leaderships or helped them develop the forms of leaderships they already had. However, for the other ones, the city lessened that potential, and they had to start a fight for survival, doing what the other women were doing: taking part in the informal economy, which is what is available to women who lived in the working-class neighbourhoods of the city.”</p><p><i>Woman, victim</i></p>`,
+    es: `<p>«Yo conocí el coraje de mi madre, su fortaleza. Tengo un recuerdo de un comandante de la Escuela de Artillería. Cuando él caminaba, a su paso todos quedaban como estatuas, como si fuera... no sé, una persona con un don. Pero cuando llegaba adonde estaba mi madre, ella tenía la fortaleza para decirle: “Usted es uno de los responsables de la muerte de mi familia. ¡Usted mató a mis hijas, usted mató a mis niñas, usted mató a mis hijos, usted mató a mi familia!”. Tenía la fortaleza para encararlo y nunca dudó en decirle que ellos eran los responsables. Ella nunca apuntó para otro lado que no fuera ese: que los habían matado los militares. A mí me costaba un montón entender por qué estábamos en esa situación. Ella tenía el coraje, la certeza y la valentía de decirles».</p><p><i>Mujer, mestiza, lideresa de la UP, víctima de exilio</i></p>`,
+    fr: `<p>« J’ai connu le courage de ma mère, sa force. J’ai souvenir d’un commandant de l’École d’Artillerie. Quand il marchait, à son passage, toutes les personnes restaient immobiles, comme des statues. Comme s'il était...je ne sais pas, une personne avec un don. Mais quand il s’approchait de ma mère, elle avait la force de lui dire : “Vous êtes l’un des responsables de la mort de ma famille. Vous ! Vous avez tué mes filles ! Vous avez tué mes filles, mes filles bien-aimées ! Vous avez tué mes fils ! Vous avez tué ma famille !” Elle avait la force de lui faire face, elle n’a jamais eu peur de dire aux militaires qu’ils étaient les responsables de la mort de notre famille. Elle n’a jamais pointé du doigt qui que ce soit d’autre qu’eux : c’étaient les militaires qui les avaient tués. À cette époque-là, pour moi, c’était difficile de bien comprendre la situation. Mais maintenant, je comprends que ma mère avait le courage, la volonté et la force de leur dire la vérité. »</p><p><i>Femme, métisse, leader du UP, victime d’exile</i></p>`,
+    en: `<p>Daniela said, “I knew my mother’s courage, her strength. I remember that there was a commander at the Escuela de Artillería of the Army. When he walked-by, people used to be paralyzed, like he had a power over them. But, when he got close to my mom, she had the strength to say to him: ‘You are one of the people responsible for my family’s death. You killed my daughters, you killed little girls! You killed my sons! You killed my family!’ She had the strength to face him, and she had no doubt that the military were the ones responsible for her family’s death. She never said anything different from this: the military had killed them. I didn’t understand why we were in that situation, but she was courageous, confident, and brave enough to tell them.”</p><p><i>Woman, </i>mestiza<i>, leader of <b><u>{1}UP{/1}</u></b>, exile victim</i></p>`,
   },
   tooltips: {
     1: {
-      es: `<p>Nota de ejemplo para la crónica.</p>`,
-      fr: `a`,
-      en: `a`,
+      es: `<p>Nota de ejemplo para la crónica T3-41.</p>`,
+      fr: `<p><strong>UP (Unión Patriótica)</strong></p>
+      <p>L'UP a participé aux élections à partir de 1986 et a remporté une grande partie des voix. Cependant, au cours des années suivantes, des milliers de membres de l'UP, dont trois des candidats à la présidence du parti, ont été victimes, entre autres, de disparitions forcées, de massacres, d'exécutions extrajudiciaires, d'assassinats, de menaces, d'attaques, de divers actes de stigmatisation, de poursuites judiciaires abusives, de tortures et de déplacements forcés.</p>
+      <p><a href="https://reliefweb.int/report/colombia/genocide-political-party-union-patriotica-over-6000-members-killed-1980s-and-1990s" target="_blank" rel="noopener noreferrer">ReliefWeb</a></p>
+	  <p><a href="https://www.britannica.com/topic/Patriotic-Union-political-party-Colombia" target="_blank" rel="noopener noreferrer">Britannica</a></p>`,
+      en: `<p><strong>UP (Unión Patriótica)</strong></p>
+      <p>The UP participated in elections beginning in 1986 and won a large portion of the votes. In subsequent years, however, thousands of UP members, including three of the party’s presidential candidates, were forcibly disappeared, massacred, extrajudicially executed, murdered and suffered, threats, attacks, various acts of stigmatization, improper prosecutions, torture, forced displacement, among other things.</p>
+      <p><a href="https://reliefweb.int/report/colombia/genocide-political-party-union-patriotica-over-6000-members-killed-1980s-and-1990s" target="_blank" rel="noopener noreferrer">ReliefWeb</a></p>
+	  <p><a href="https://www.britannica.com/topic/Patriotic-Union-political-party-Colombia" target="_blank" rel="noopener noreferrer">Britannica</a></p>`,
     },
   },
   contextInfo: {
-    es: `<p>Lugar: Bogotá</p><p>Año: N/A</p><p>Hecho(s) victimizante(s): desplazamiento forzado</p><p>Actor armado: N/A</p>`,
-    fr: `<p>Lieu : Bogotá</p><p>Date : N/A</p><p>Fait(s) victimisant(s) : déplacement forcé</p><p>Acteur armé : N/A</p>`,
-    en: `<p>Place: Bogotá</p><p>Date: N/A</p><p>Victimizing act(s): forced displacement </p><p>Armed actor: N/A</p>`,
+    es: `<p>Lugar: N/A</p><p>Año: N/A</p><p>Hecho(s) victimizante(s): exilio, asesinato</p><p>Actor armado: Escuela de Artillería</p>`,
+    fr: `<p>Lieu : N/A</p><p>Date : N/A</p><p>Fait(s) victimisant(s) : exile, assassinat</p><p>Acteur armé : l’École d’Artillerie</p>`,
+    en: `<p>Place: N/A</p><p>Date: N/A</p><p>Victimizing act(s): exile, murder</p><p>Armed actor: Army</p>`,
   },
 };
