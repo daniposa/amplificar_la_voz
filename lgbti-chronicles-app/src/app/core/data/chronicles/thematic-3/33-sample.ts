59d0cd0a892acc5efdd8d34916bc9ce5a0a39bd0
sergiopaz-bit <sergio.paz@udea.edu.co>
2026-05-28 18:27:34 -0500

    Update 33-sample.ts

--- lgbti-chronicles-app/src/app/core/data/chronicles/thematic-3/33-sample.ts
+++ lgbti-chronicles-app/src/app/core/data/chronicles/thematic-3/33-sample.ts
@@ -2,25 +2,42 @@ import type { LocalizedModalContent } from '../../../models/content.model';
 
 export const CHRONICLE_T3_33: LocalizedModalContent = {
   title: {
-    es: 'Karol',
-    fr: 'Karol',
-    en: 'Karol',
+    es: 'Marianela',
+    fr: 'Marianela',
+    en: 'Marianela',
   },
   rawText: {
-    es: `<p>«Llegaron los helicópteros, ametrallaron y ahí nos mataron a una guerrillera. Yo me tropecé con un paramilitar muerto, con su cabeza; me caí y lo vi ensangrentado. Eso fue espantoso para mí, pero fue peor cuando vi a unas paramilitares a las que les habían rajado los senos con machete, les cortaron los senos, y otros guerrilleros estaban jugando con la cabeza de un paramilitar».</p><p><i>Mujer, excombatiente de las FARC-EP</i></p>`,
-    fr: `<p>« Les hélicoptères sont arrivés et ils ont mitraillé notre compagne guérillera. J’ai trébuché sur le corps d’un paramilitaire, sur sa tête. Je suis tombée, et je l’ai vu couvert de sang. Ç'a été horrible pour moi, surtout quand j’ai vu des femmes à qui on avait coupé les seins à la machette, ses seins ont été coupés, et plus loin, d’autres guérilleros jouaient avec la tête d’un paramilitaire. »</p><p><i>Femme ex-combattante des FARC-EP</i></p>`,
-    en: `<p>“Some helicopters arrived and strafed the camp, then they killed a guerrillera from our group. I tripped over a dead paramilitay’s head. I fell and saw him all covered in blood. That was terrifying for me but the worst part was when I saw some female paramilitaries whose breasts had been slit with machetes, their breast had been amputated, and other male guerrilla members were playing with a paramilitary’s head.”</p><p><i>Female former combatant</i></p>`,
+    es: `<p>«Pues en mi época de violencia, como de los noventa hacia acá, yo recuerdo que el Estado entró no como institución, sino con la represión del Ejército. Entonces, hubo mucha violencia hacia los campesinos con torturas, encarcelamientos, con la estigmatización de que éramos guerrilleros y uno ni sabía por qué existía el conflicto. El Ejército llegaba a la casa y era traumático escuchar disparos, escuchar helicópteros era terrible; me acuerdo que hasta los animales, el páramo se ponía frío, se ponía gris, era una cosa que uno no entendía.... En mi caso, mi madre y yo vivíamos en la finca y todos se desplazaron, pero ella tuvo la valentía de decir no, yo no me desplazo, primero porque las condiciones económicas no daban para darnos otro lugar y segundo, porque ella dijo: “Yo no debo nada, mis hijos no deben nada; entonces por qué me voy a ir del territorio”. Ahí estuvo la resistencia. Teníamos miedo, toda la situación económica y social, pero no nos íbamos a ir. Ese fue uno de los aprendizajes que me llevo de esa época: a pesar de los malos momentos hay que resistir y quedarse en el territorio».</p><p><i>Mujer víctima del conflicto</i></p>`,
+    fr: `<p>Marianela a partagé son histoire : « Bon, je me rappelle que quand j’étais jeune, vers les années quatre-vingt-dix, l’État est arrivé sur notre territoire, pas sous la forme d’une institution, mais plutôt sous la forme d’une répression exercée par l’armée. Donc, à ce moment-là, il y a eu beaucoup de violence envers nous, les paysans : l’armée nous torturait, nous emprisonnait, nous stigmatisait en nous disant que nous étions des guérilleros, même si nous ne connaissions pas le conflit. L’armée arrivait chez nous et commençait à tirer. C’était vraiment horrible d’entendre les coups de feu et les hélicoptères voler. Les animaux avaient peur eux aussi, le <i><b>{1}páramo{/1}</b></i> devenait gris et froid. Nous ne comprenions pas la situation. Par exemple, ma mère et moi habitions à la <i><b>{2}finca{/2}</b></i> et nous avons vu les gens partir. Mais ma mère a eu le courage de résister : “Moi, je ne vais pas partir.”  Nous n’avions pas d’argent pour trouver une autre maison, et ma mère a dit : “Je ne dois rien à personne et mes enfants non plus, alors pourquoi devrais-je quitter le territoire ?” Voici un des enseignements qui m’est resté de cette époque, j’ai appris à résister quoiqu'il arrive et à ne pas quitter le territoire quelle qu’en soit la situation. »</p><p><i>Femme victime du conflit</i></p>`,
+    en: `<p>“In the period of violence that I lived through, from the nineties on, I remember that the government got into the territory not as a figure of institutionality but as a repressive force headed by the Army. Then there was a lot of violence against farmers, torture, and imprisonment, because they used to stigmatize us as guerrilla members and we didn’t know the reasons for the conflict. The Army would appear in the houses and traumatize all of us with their shots and helicopters. It was horrible! I remember how the animals and even the <i><b>{1}páramo{/1}</b></i> looked colder, darker. We didn’t understand the situation… My mother and I used to live in the <i><b>{2}finca{/2}</b></i> and we saw how people fled  the territory, but not my mother, she was brave enough to  say, ‘no, I won’t leave,’, first, because our financial situation wouldn’t allow us to get another place; and second, because she used to say, ‘If I don’t owe anybody anything and neither do my children, then why  should I leave the territory?’ That was resistance. We were afraid of the financial and social issues, but we wouldn’t go anywhere. That is one of the main learnings I have from that time: that even at bad moments, we must resist and stay on the land.”</p><p><i>Woman, victim of the conflict</i></p>`,
   },
   tooltips: {
     1: {
       es: `<p>Nota de ejemplo para la crónica T3-33.</p>`,
-      fr: `<p>Note d’exemple pour la chronique T3-33.</p>`,
-      en: `<p>Sample tooltip for chronicle T3-33.</p>`,
+      fr: `<p><strong><i>Páramo</i></strong></p>
+      <p><img src="images/flotantes/paramo.avif" alt="Páramo" width="220"></p>
+<p><small>Une <i>páramo</i> en Colombie</small></p>
+<p><a href="https://colombia-travels.com/blog/tourism/paramo" target="_blank" rel="noopener noreferrer">Informations supplémentaires</a></p>`,
+      en: `<p><strong><i>Páramo</i></strong></p>
+      <p><img src="images/flotantes/paramo.avif" alt="Páramo" width="220"></p>
+<p><small>A <i>páramo</i> in Colombia</small></p>
+<p><a href="https://colombia-travels.com/blog/tourism/paramo" target="_blank" rel="noopener noreferrer">More information</a></p>`,
+    },
+    2: {
+      es: `<p>Nota de ejemplo para la crónica T3-33.</p>`,
+      fr: `<p><strong><i>Finca</i></strong></p>
+<p><img src="images/flotantes/finca.avif" alt="Une <i>finca</i> en Antioquia" width="220"></p>
+<p><small>Une <i>finca</i> en Antioquia</small></p>
+<p>En Colombie, le terme « finca » est habituellement utilisé pour désigner une maison, quel qu’en soit la taille, située dans une zone rurale. Une <i>finca</i> peut désigner aussi bien une petite maison de campagne comme une grande maison avec ses propres terres agricoles. Ces terres agricoles peuvent être des grandes étendues utilisées générer des bénéfices importants, ou elles peuvent être des petits potagers. Cela peut aussi être des terrains vagues, n’ayant pas d’utilité particulière mise à part d’être un lieu où l’on peut passer le temps hors de la ville.</p>`,
+      en: `<p><strong><i>Finca</i></strong></p>
+<p><img src="images/flotantes/finca.avif" alt="A <i>finca</i> in Antioquia" width="220"></p>
+<p><small>A <i>finca</i> in Antioquia</small></p>
+<p>In Colombia, “finca” is a term commonly used to refer to a house, regardless of its size, located in a rural area. A <i>finca</i> can range from a small country house to a large house with its own plantation. These agricultural plantations can be large expanses of land intended to produce a significant profit, or they can be small vegetable gardens. They may also be empty plots of land that serve no particular purpose, except to provide a place to spend time outside of any city or village.</p>`,
     },
   },
   contextInfo: {
-    es: `<p>Lugar: Sabanas del Yarí (Meta, Caquetá)</p><p>Año: N/A</p><p>Hecho(s) victimizante(s): violencia sexual, asesinato, violencia psicológica</p><p>Actor armado: paramilitares</p>`,
-    fr: `<p>Lieu : Sabanas del Yarí (Meta, Caquetá)</p><p>Date : N/A</p><p>Fait(s) victimisant(s) : violence sexuelle, assesinat, violence psicologique</p><p>Acteur armé : paramilitaires</p>`,
-    en: `<p>Sabanas del Yarí (Meta, Caquetá)</p><p>Date: N/A</p><p>VVictimizing act(s): sexual violence, murder, psicological violence</p><p>Armed actor: paramilitary group</p>`,
+    es: `<p>Lugar: N/A</p><p>Año: N/A</p><p>Hecho(s) victimizante(s): estigmatización, tortura, libertad de residir y circular</p><p>Actor armado: Estado</p>`,
+    fr: `<p>Lieu : N/A</p><p>Date : N/A</p><p>Fait(s) victimisant(s) : stimagtisation, torture, liberté de résider et de circuler</p><p>Acteur armé : l’État</p>`,
+    en: `<p>Place: N/A</p><p>Date: N/A</p><p>VVictimizing act(s): stigmatization, torture, freedom of movement and residence</p><p>Armed actor: State forces</p>`,
   },
 };
