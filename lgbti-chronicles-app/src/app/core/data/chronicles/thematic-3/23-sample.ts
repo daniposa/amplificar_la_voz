feef52c672e8ed75c017f7d6bec4837b51760195
sergiopaz-bit <sergio.paz@udea.edu.co>
2026-05-28 16:29:39 -0500

    Update 23-sample.ts

--- lgbti-chronicles-app/src/app/core/data/chronicles/thematic-3/23-sample.ts
+++ lgbti-chronicles-app/src/app/core/data/chronicles/thematic-3/23-sample.ts
@@ -2,29 +2,43 @@ import type { LocalizedModalContent } from '../../../models/content.model';
 
 export const CHRONICLE_T3_23: LocalizedModalContent = {
   title: {
-    es: 'Magali',
-    fr: 'Magali',
-    en: 'Magali',
+    es: 'Dioselina',
+    fr: 'Dioselina',
+    en: 'Dioselina',
   },
   rawText: {
-    es: `<p>Magali, una excombatiente del M-19 se refirió a un momento histórico en Colombia:</p>  
-<p>«Yo pienso que en ese momento era legítima la guerrilla como defensa del pueblo; en retrospectiva, la única forma de hacerse oír políticamente en este país era mediante la lucha armada. Eso era lo que uno veía. Hoy, si me preguntan, yo diría que no, que alguien tiene que quebrarle el pescuezo al ciclo de la violencia, alguien tiene que encontrar una forma distinta de dirimir los conflictos, pero en ese momento era lo legítimo. Entonces yo, para concluir, digo: para mí la insurgencia era una opción política y tenía que ver con el derecho a la rebelión contra un orden establecido que va contra el bien común, la solidaridad del bien común».</p><p><i>Mujer, excombatiente del M-19</i></p>`,
-    fr: `<p>Magali, une ex-combattante du <b><u>{1}M-19{/1}</u></b> a fait référence à un moment historique en Colombie :</p>  
-
-<p>« Je pense qu’à ce moment-là, la guérilla était un mouvement légitime pour défendre le peuple ; en rétrospective, la lutte armée était la seule manière de se faire entendre politiquement dans ce pays. C’est ainsi qu’on le percevait. Aujourd'hui, si vous me posiez la question, je dirais que non, que quelqu'un casser le cou au cycle de la violence, quelqu’un doit trouver une autre manière de résoudre les conflits mais, à ce moment-là, c’était légitime. Alors moi, pour conclure je dis que pour moi l’insurgence était une option politique en relation avec le droit à la rébellion contre un ordre établi qui va à l’encontre du bien commun, la solidarité du bien commun. »</p><p><i>Femme ex-combattante du M-19.</i></p>`,
-    en: `<p>Magali, <b><u>{1}M-19{/1}</u></b> former combatant, referred to an historical moment in Colombia:</p><p>“I think, back then, the guerrilla were a legitimate way for people to defend themselves. In retrospect, the only way of making yourself politically heard in this country was through armed struggle. That’s the way things were. Today, if you asked me, I would say, no, I would say that someone has to break the violence cycle’s neck, someone has to find a different way of resolving conflicts. But, at that moment, it was the legitimate thing to do. Then, to conclude, I’d say, ’for me, insurgence was a political option, it had to do with the right of rebellion against an established order that goes against common good, the solidarity of the common good.”</p><p><i>M-19 former female combatant</i></p>`,
+    es: `<p>Así lo denunció Dioselina, campesina de Santander de Quilichao (Cauca), cuyo hijo fue asesinado por los paramilitares del Bloque Calima de las AUC en 2007. Ella confrontó a los responsables con las manos desnudas, pero con mucha claridad: «Nosotros teníamos una parcela por Toribío, con muchas cosas sembradas allá; todos los días íbamos a darle vuelta a la finca. Cuando llegamos y abrimos la puerta estaba esa gente allá y tenían un señor amarrado. Entonces les toqué la puerta y les dije: “Buenos días, me hacen el favor y me desocupan, que esto no es pa fechoría de ustedes. Me respetan mi finca”. Y ellos: “Ay, vieja vagabunda, es que esto no es que le respetemos”. “Vagabunda la madre suya que lo mandó acá, pero a mí me respetan o nos vamos a matar, hábleme claro”. Entonces me dijo mi hermanita: “No, hermanita, vámonos. Dejemos esa gente aquí”. Y le dije: “No me voy hasta que se vayan”. No tenía miedo de que me mataran, si ya me habían matado a mi hijo».</p><p><i>Mujer, campesina, afrocolombiana</i></p>`,
+    fr: `<p>En 2007, les paramilitaires du <b><u>{1}Bloque Calima des AUC{/1}</u></b> ont assassiné le fils de Dioselina une paysanne de Santader de Quilichao (Cauca) qui a expliqué à la Commission comment elle a fait face aux responsables de la mort de son fils : « Ma famille avait une parcelle de terre à Toribío, une <b><u><i>{2}finca{/2}</i></u></b>. Là, nous semions de tout. Nous n’y habitions pas, mais nous y allions tous les jours pour vérifier que tout allait bien. Un jour, quand nous sommes arrivées et avons ouvert la porte, ces gens étaient là. Ils avaient attaché un homme avec quelques cordes. Donc j’ai frappé la porte et les ai avertis : “Hé oh ! Je vous demande de quitter ma propriété. Ce n’est pas ici que vous allez commettre vos délits. Respectez ma propriété.” Ils ont alors répondu : “Du respect ? Tais-toi, salope !  Ça n’a rien à voir avec le respect.” “Ta mère la salope qui t’a envoyé ici. Respecte-moi. Il vaut mieux que vous vous en alliez ou on va se tuer. C’est clair ?” Mais ma petite sœur m’a dit : “Non, non, il vaut mieux partir. Il vaut mieux partir.” Et j’ai répondu : “Bah, non. Je vais rester jusqu'à ce qu’ils quittent notre maison.” Je n’ai pas peur d’être assassinée. Après tout, ils avaient déjà assassiné mon fils bien-aimé. »</p><p><i>Femme paysanne et afro-colombienne</i></p>`,
+    en: `<p>Dioselina, a farmer from Santander de Quilichao (Cauca), whose son had been killed by paramilitary forces from <b><u>{1}Bloque Calima of the AUC{/1}</u></b> in 2007, stated like this: “We had some land in Toribío with many crops. Everyday, we would go there to see how things were going. One day, when we arrived and opened the door, we saw those people there. They had tied a man up. So, I knocked on the door, and told them: ‘Good morning, do me a favor and get out. This place isn’t for you to commit crimes. Have some respect for my <b><u><i>{2}finca{/2}</i></u></b>.’ And they replied: ‘Listen, old bitch. This isn’t about respect.’ ‘The old bitch is your mother who brought you here. Show me some respect or we’ll kill each other here, you understand?’ So, my little sister told me: ‘Please don’t, sister, let’s go. Leave those people here.’ But I said, ‘No, I won’t leave until they get out of here". I wasn’t afraid of being killed, they had already killed my son.’.”</p><p><i>Woman, farmer, Afro-colombian</i></p>`,
   },
   tooltips: {
     1: {
-      es: `<p>Nota de ejemplo para la crónica T3-23.</p>`,
-      fr: `<p><strong>M-19</strong></p>
-      <p>Le M-19, également connu sous le nom de Mouvement du 19 avril ou Movimiento 19 de Abril en espagnol, était un groupe de guérilla urbaine actif en Colombie dans les années 1970 et 1980. Le M-19 de Colombie était le deuxième plus grand groupe de guérilla du pays dans les années 1980, période de son activité maximale. Il s'est développé comme une forme de résistance au Frente nacional, un accord non démocratique entre les principaux partis politiques colombiens visant à partager le pouvoir en alternant le contrôle du gouvernement.</p>
-      <p><a href="https://study.com/academy/lesson/m-19-history-members-facts-19th-april-movement.html" target="_blank" rel="noopener noreferrer">Study.com</a></p>`,
-      en: `<p><strong>M-19</strong></p>
-      <p>The M-19 also known as the 19th of April Movement and April 19 Movement, or Movimiento 19 de Abril in Spanish, was an urban guerrilla group active in Colombia in the 1970s and 1980s. The M-19 of Colombia was the second largest guerrilla group in Colombia during the 1980s, its time of peak activity. It developed as a form of resistance to the National Front, a non-democratic agreement of the main Colombian political parties to share power through alternating control of the government.</p>
-      <p><a href="https://study.com/academy/lesson/m-19-history-members-facts-19th-april-movement.html" target="_blank" rel="noopener noreferrer">Study.com</a></p>`,
+      es: `<p>Nota de ejemplo para la crónica T3-41.</p>`,
+      fr: `<p><strong>Bloque Calima des AUC</strong></p>
+      <p>Il s'agit d'un groupe paramilitaire affilié aux Forces d'autodéfense unies de Colombie (AUC) qui a opéré dans les départements du Valle del Cauca et du Cauca, ainsi que dans certaines municipalités du Quindío et du Huila entre 1999 et 2004, année de sa démobilisation collective. À partir du milieu de l'année 1999, le groupe paramilitaire a concentré ses actions dans la partie centrale de Valle del Cauca et a commis des actes de violence isolés dans d'autres zones du département, ainsi que dans les municipalités de Génova et Pijao dans le Quindío.</p>
+      <p><a href="https://centrodememoriahistorica.gov.co/wp-content/uploads/2020/02/bloque-calima-auc.pdf " target="_blank" rel="noopener noreferrer">Centro de Memoria Histórica</a></p>`,
+      en: `<p><strong>Bloque Calima of the AUC</strong></p>
+      <p>It was a paramilitary group affiliated with the United Self-Defense Forces of Colombia (AUC) that operated in the departments of Valle del Cauca and Cauca, as well as in some municipalities of Quindío and Huila between 1999 and 2004, the year of its collective demobilization. From mid-1999, the paramilitary group focused its actions in the central part of Valle del Cauca and committed isolated acts of violence in other areas of the department, as well as in the municipalities of Génova and Pijao in Quindío.</p>
+      <p><a href="https://centrodememoriahistorica.gov.co/wp-content/uploads/2020/02/bloque-calima-auc.pdf " target="_blank" rel="noopener noreferrer">Centro de Memoria Histórica</a></p>`,
+    },
+	2: {
+      es: `<p>Nota de ejemplo para la crónica T3-41.</p>`,
+      fr: `<p><strong><i>Finca</i></strong></p>
+		<p><img src="images/flotantes/finca.avif" alt="Une <i>finca</i> en Antioquia" width="220"></p>
+		<p><small>Une <i>finca</i> en Antioquia</small></p>
+		<p>En Colombie, le terme « finca » est habituellement utilisé pour désigner une maison, quel qu’en soit la taille, située dans une zone rurale. Une <i>finca</i> peut désigner aussi bien une petite maison de campagne comme une grande maison avec ses propres terres agricoles. Ces terres agricoles peuvent être des grandes étendues utilisées générer des bénéfices importants, ou elles peuvent être des petits potagers. Cela peut aussi être des terrains vagues, n’ayant pas d’utilité particulière mise à part d’être un lieu où l’on peut passer le temps hors de la ville.</p>`,
+	  en: `<p><strong><i>Finca</i></strong></p>
+		<p><img src="images/flotantes/finca.avif" alt="A <i>finca</i> in Antioquia" width="220"></p>
+		<p><small>A <i>finca</i> in Antioquia</small></p>
+		<p>In Colombia, “finca” is a term commonly used to refer to a house, regardless of its size, located in a rural area. A <i>finca</i> can range from a small country house to a large house with its own plantation. These agricultural plantations can be large expanses of land intended to produce a significant profit, or they can be small vegetable gardens. They may also be empty plots of land that serve no particular purpose, except to provide a place to spend time outside of any city or village.</p>`,
     },
   },
+  contextInfo: {
+    es: `<p>Lugar: Santander de Quilichao, Cauca</p><p>Año: 2007</p><p>Hecho(s) victimizante(s): asesinato, expropiación de tierras</p><p>Actor armado: Bloque Calima de las AUC</p>`,
+    fr: `<p>Lieu : Santander de Quilichao, Cauca</p><p>Date : 2007</p><p>Fait(s) victimisant(s) : assassinat, dépossession de terres</p><p>Acteur armé : Bloque Calima des AUC</p>`,
+    en: `<p>Place: Santander de Quilichao, Cauca</p><p>Date: 2007</p><p>Victimizing act(s): sexual violence</p><p>Armed actor: 32nd Front of FARC-EP</p>`,
+  },
+};
   contextInfo: {
     es: `<p>Lugar: N/A</p><p>Año: N/A</p><p>Hecho(s) victimizante(s): N/A</p><p>Actor armado: N/A</p>`,
     fr: `<p>Lieu : N/A</p><p>Date : N/A</p><p>Fait(s) victimisant(s) : N/A</p><p>Acteur armé : N/A</p>`,
