18a23826f2fc929d1f2fd3cde0ba1738a1b3d665
sergiopaz-bit <sergio.paz@udea.edu.co>
2026-05-28 18:14:12 -0500

    Update 32-sample.ts

--- lgbti-chronicles-app/src/app/core/data/chronicles/thematic-3/32-sample.ts
+++ lgbti-chronicles-app/src/app/core/data/chronicles/thematic-3/32-sample.ts
@@ -2,29 +2,47 @@ import type { LocalizedModalContent } from '../../../models/content.model';
 
 export const CHRONICLE_T3_32: LocalizedModalContent = {
   title: {
-    es: 'Violeta',
-    fr: 'Violeta',
-    en: 'Violeta',
+    es: 'Betty',
+    fr: 'Betty',
+    en: 'Betty',
   },
   rawText: {
-    es: `<p>«Me desaparecieron a mi hijo porque él no se quería meter en el cuento [...] le insistieron tanto que un día llegaron y le dijeron que les hiciera una carrera y a mi hijo nunca más lo vi. Él era motorratón. Para mí es muy fuerte porque el negro nunca va a salir de ser esclavo, nunca lo van a dejar surgir y lo más triste, que nos ponen a matarnos negros contra negros, como animales sin corazón, sin alma».</p><p><i>Red Mariposas de Alas Nuevas Construyendo Futuro, «Las espinas del racismo»</i></p>`,
-    fr: `<p>« Ils ont fait disparaître mon fils, mon propre fils., mon fils bien-aimé… Il ne voulait pas s’impliquer avec eux [...] ils ont tellement insisté qu’un jour, ils lui ont demandé de leur rendre service avec sa moto, et je ne l’ai plus jamais revu. Il faisait le taxi avec sa moto. Pour moi, c’est quelque chose de très dur, parce que les Noirs ne pourront jamais sortir de l’esclavage, on ne les laisse jamais aller de l’avant, et ce qui est encore plus triste, c’est qu’on nous a mis à nous tuer entre nous, à tuer les nôtres, comme des animaux, sans cœur, sans âme. »</p><p><i><b><u>{1}Red Mariposas de Alas Nuevas Construyendo Futuro{/1}</u></b>, « les épines du racisme »</i></p>`,
-    en: `<p>“My beloved son was disappeared because he didn’t want to be involved in those things [...] They kept going about it for a long time, until one day they asked him to take them somewhere and I never saw my son again. He drove a motorcycle-taxi. It was hard for me because I realized black people will never leave slavery behind, they will never let us advance, and the hardest part is that they make us kill each other, black people against black people, just like heartless and soulless animals.”</p><p><i><b><u>{1}Red Mariposas de Alas Nuevas Construyendo Futuro{/1}</u></b>, «The thorns of racism»</i></p>`,
+    es: `<p>El acompañamiento continuo, la escucha y el respeto por otras mujeres hicieron que Las Tamboreras del Cauca se apoyaran entre sí para exigir sus derechos y acceder a la justicia, como lo manifestó Catalina: «Cuando Ángela me dijo eso, yo busqué a Nancy y le dije: “Mira lo que está pasando, yo me acuerdo cuando te pasó. Hay una posibilidad de que nos ayuden. A mí también me pasó, pero que se quede entre las dos”. Entonces ella me dijo: “Pues yo porque a mí me pasó, y a fulana y a fulana”. Poco a poco, entre todas empezamos a unir cabos, a dialogar, y le dije a Ángela: “Hay veinte mujeres”, y había una que había conseguido otras mujeres. Así se hizo la jornada con la Fiscalía, la Unidad de Víctimas, la Defensoría del Pueblo y la Red de Mujeres Víctimas y Profesionales. En ese momento se llamaba “Mujer sigue mis pasos”».</p><p><i>Mujer, lideresa, mestiza</i></p>`,
+    fr: `<p>L’accompagnement continu, l’écoute et le respect par les autres femmes ont fait que <b>{1}Las Tamboreras del Cauca{/1}</b> se sont entraidées pour demander leurs droits et obtenir justice. Catalina, membre de ce groupe des femmes a raconté : « Quand Angela m’a parlé de la situation, j’ai parlé avec Nancy et je lui ai dit : “Écoute, ce qui se passe en ce moment, je me souviens quand ça t’ait arrivé, comme moi mais ne dis rien. Il y a des personnes qui peuvent peut être nous aider.” Alors, elle m’a répondu : “Bon, je sais que, comme moi, il y en a deux autres femmes a qui s’est arrivé.” Peu à peu, nous avons contacté d’autres femmes ; l’une d’entre elles connaissait d’autres victimes et nous avons connecté les pièces du puzzle. J’ai dit à Angela “il y a 20 femmes.” Ainsi nous avons pu faire une réunion avec le Parquet, l’<b>{3}Unité de Victimes{/3}</b>, le Bureau du défenseur des droits et le <b>{2}Réseau de femmes victimes et professionnelles{/2}</b> à ce moment-là, le réseau s’appelait Mujer Sigue mis Pasos (Femme, suis mes pas) ».</p><p><i>Femme leader métisse </i></p>`,
+    en: `<p>Continuous backing, listening and respect for other women is what made it possible for <b>{1}Las Tamboreras del Cauca{/1}</b>, to support each other in their fight for their rights and for justice, as Catalina stated, “When Ángela told me about it, I looked for Nancy and I told her, ‘Look at what’s going on. I remember when that happened to you. Maybe they can help us. It happened to me too but don’t tell anyone.’ So, she told me, ‘yes, it happened to me, and to other women’. Soon enough, we all started to talk and to put two and two together. Then, I told Ángela ‘there are twenty women.’ But one of them brought other women. So, we gathered with the Attorney General’s Office, the <b>{3}Victims Unit{/3}</b>, the Ombudsperson’s Office, and the <b>{2}Network of Victims and Professional Women{/2}</b>, which was known as ‘Woman, follow my steps’ (Mujer, sigue mis pasos) at that time.”</p><p><i>Woman, leader, </i>mestiza</p>`,
   },
   tooltips: {
     1: {
       es: `<p>Nota de ejemplo para la crónica T3-32.</p>`,
-      fr: `<p><strong>Red  Mariposas de Alas Nuevas</strong></p>
-      <p>Le Red Mariposas de Alas Nuevas Construyendo Futuro est un groupe de femmes qui défendent les droits et la vie à Buenaventura. Elles apportent leur soutien à d'autres femmes ayant subi des violences. Ils guérissent grâce au <i>comadreo</i>, à un rassemblement collectif et à leurs traditions ancestrales.</p>
-      <p><a href="https://redmariposasakina.org/" target="_blank" rel="noopener noreferrer">Site officiel</a></p>`,
-      en: `<p><strong>Red  Mariposas de Alas Nuevas</strong></p>
-      <p>Red Mariposas de Alas Nuevas: The Red Mariposas de Alas Nuevas Construyendo Futuro is a group of women who defend rights and life in Buenaventura. They support other women who have experienced situations of violence. They heal through <i>comadreo</i>, collective gathering, and their ancestral traditions.</p>
-      <p><a href="https://redmariposasakina.org/" target="_blank" rel="noopener noreferrer">Official website</a></p>`,
+      fr: `<p><strong>Tamboreras del Cauca</strong></p>
+      <p>Il s’agit d’une initiative collective qui partage leur chemin vers la guérison à travers l’usage de percussions et de pièces de théâtre, créée dans le contexte d’un programme psychologique, ce qui peut être un cas d’étude. Ce parcours prend en compte les dispositifs personnels, communautaires et culturels. Elle recourt à des rituels et à des métaphores qui pourraient servir de scénario pour comprendre le parcours des victimes et contribuer à un changement de cadre qui aborde les expériences traumatiques à partir des récits des victimes, en vue d'une guérison collective et, à terme, d'une réconciliation de la communauté.</p>
+      <p><a href="https://scispace.com/pdf/mapping-a-journey-of-healing-towards-reconciliation-the-4nhb8vj7o8.pdf" target="_blank" rel="noopener noreferrer">Source</a></p>
+      <p><a href="https://www.youtube.com/watch?v=FSIOYdpczQw" target="_blank" rel="noopener noreferrer">Video</a></p>`,
+      en: `<p><strong>Tamboreras del Cauca</strong></p>
+      <p>It is a collective initiative that shares their healing journey through the use of drums and the performance of a theatre play, created in the context of a psychosocial program, can work as a case to explore. This journey takes into account personal, communal, and cultural devices. It makes use of rituals, and metaphors that could be the scenario to understand victims´ journey and the input to a framework shift that addresses trauma experiences from victims´ narratives towards collective healing and, eventually, community reconciliation.</p>
+      <p><a href="https://scispace.com/pdf/mapping-a-journey-of-healing-towards-reconciliation-the-4nhb8vj7o8.pdf" target="_blank" rel="noopener noreferrer">Source</a></p>
+      <p><a href="https://www.youtube.com/watch?v=FSIOYdpczQw" target="_blank" rel="noopener noreferrer">Video</a></p>`,
+    },
+    2: {
+      es: `<p>Nota de ejemplo para la crónica T3-32.</p>`,
+      fr: `<p><strong>Red de Mujeres Víctimas y Profesionales</strong></p>
+      <p>Il s’agit d’une organisation de femme qui ont été victimes de violences sexuelles pendant le conflit armé, agissant dans différentes régions du pays. Le réseau a été fondé en 2016 à travers des initiatives de plusieurs femmes qui se sont battues pour les droits des femmes, et plus particulièrement les droits des victimes de violences sexuelles.</p>`,
+      en: `<p><strong>Red de Mujeres Víctimas y Profesionales</strong></p>
+      <p>It is an organization of women who were victims of sexual violence in the armed conflict, operating in various regions of the country. It was founded in 2016 through the initiative of several women who have fought for women’s rights, especially those of victims of sexual violence.</p>`,
+    },
+    3: {
+      es: `<p>Nota de ejemplo para la crónica T3-32.</p>`,
+      fr: `<p><strong>L’Unité des victimes</strong></p>
+      <p>L’Unité des victimes (Unidad de Víctimas) vise à rapprocher l’État des victimes grâce à une coordination efficace et à des actions transformatrices qui favorisent la participation active des victimes dans leur processus de réparation. Elle est chargée de coordonner l'assistance, de la prise en charge des victimes et des mesures de réparation fournies par l'État, ainsi que de relier les organismes qui font partie du Système national d'assistance et de réparation globales aux victimes.</p>
+      <p><a href="https://www.unidadvictimas.gov.co/resena-de-la-unidad/" target="_blank" rel="noopener noreferrer">Site officiel</a></p>`,
+      en: `<p><strong>Victims Unit</strong></p>
+      <p>It seeks to bring the State closer to victims through efficient coordination and transformative actions that promote the effective participation of victims in their reparation process. It is responsible for coordinating the assistance, care, and reparation measures provided by the State, as well as for connecting the entities that are part of the National System for Comprehensive Attention and Reparation of Victims.</p>
+      <p><a href="https://www.unidadvictimas.gov.co/resena-de-la-unidad/" target="_blank" rel="noopener noreferrer">Official website</a></p>`,
     },
   },
   contextInfo: {
-    es: `<p>Lugar: N/A</p><p>Año: N/A</p><p>Hecho(s) victimizante(s): desaparición forzosa, racismo</p><p>Actor armado: N/A</p>`,
-    fr: `<p>Lieu : N/A</p><p>Date : N/A</p><p>Fait(s) victimisant(s) : disparition forcée, racisme</p><p>Acteur armé : N/A</p>`,
-    en: `<p>Place: N/A</p><p>Date: N/A</p><p>Victimizing act(s): forced dissapairing, racism</p><p>Armed actor: N/A</p>`,
+    es: `<p>Lugar: Popayán, Cauca</p><p>Año: N/A</p><p>Hecho(s) victimizante(s): violencia sexual</p><p>Actor armado: N/A</p>`,
+    fr: `<p>Lieu : Popayán, Cauca</p><p>Date : N/A</p><p>Fait(s) victimisant(s) : violence sexuelle</p><p>Acteur armé : N/A</p>`,
+    en: `<p>Place: Popayán, Cauca</p><p>Date: N/A</p><p>Victimizing act(s): sexual violence</p><p>Armed actor: N/A</p>`,
   },
 };
