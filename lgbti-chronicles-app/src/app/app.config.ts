fd0b600248812d3af8fae01972fe4f7930b456bb
sergiopaz-bit <sergio.paz@udea.edu.co>
2026-06-25 10:28:42 -0500

    Update app.config.ts

--- lgbti-chronicles-app/src/app/app.config.ts
+++ lgbti-chronicles-app/src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withAnchorScrolling } from '@angular/router'; // 🛠️ Añadimos withAnchorScrolling
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    // 🛠️ Pasamos la función como segundo parámetro para activar el scroll por fragmentos (#)
-    provideRouter(routes, withAnchorScrolling(),withScrollPositionRestoration('top'))
+    
+    // 🛠️ Configuración compatible con tu versión de Angular para activar el scroll por fragmentos (#)
+    provideRouter(routes, {
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'top'
+    })
   ]
 };
